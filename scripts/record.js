6431c232b424a2d5aa95d2dad2417eec9b28fa1e
Zhijiang Yang <yzjkid9@gmail.com>
2021-05-20 13:31:04 +0800

    :new: added status badge

--- scripts/record.js
+++ scripts/record.js
@@ -3,7 +3,7 @@
  * @Author: Kotori Y
  * @Date: 2021-05-19 20:55:07
  * @LastEditors: Kotori Y
- * @LastEditTime: 2021-05-19 21:22:16
+ * @LastEditTime: 2021-05-20 13:30:37
  * @FilePath: \swarming\scripts\record.js
  * @AuthorMail: kotori@cbdd.me
  */
@@ -11,6 +11,8 @@ const readme = `\
 # Swarming
 ---
 ⏰ Attacked on ${new Date().toUTCString()}
+
+[![ddos admetSar](https://github.com/kotori-y/swarming/actions/workflows/main.yml/badge.svg)](https://github.com/kotori-y/swarming/actions/workflows/main.yml)\
 
 `
 
