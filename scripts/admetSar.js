678e5f7023385fac104651a92081c219e7e77e71
Zhijiang Yang <yzjkid9@gmail.com>
2021-05-21 15:00:31 +0800

    adjusted

--- scripts/admetSar.js
+++ scripts/admetSar.js
@@ -3,7 +3,7 @@
  * @Author: Kotori Y
  * @Date: 2021-05-19 19:55:47
  * @LastEditors: Kotori Y
- * @LastEditTime: 2021-05-21 14:49:09
+ * @LastEditTime: 2021-05-21 15:00:19
  * @FilePath: \swarming\scripts\admetSar.js
  * @AuthorMail: kotori@cbdd.me
  */
@@ -11,7 +11,7 @@ const fetch = require("node-fetch");
 const load = require("./load");
 const time = require("./time");
 
-async function visit(smiles, tryTimes=1) {
+async function visit(smiles, tryTimes = 1) {
   try {
     const postUrl = "http://lmmd.ecust.edu.cn/admetsar2/result/";
     let body = {
@@ -26,10 +26,11 @@ async function visit(smiles, tryTimes=1) {
     });
     let html = await resp.text();
     console.log(JSON.parse(html)["result"]);
+    return;
   } catch (e) {
-    if (tryTimes<=5) {
-      await time.sleep(1000)
-      await visit(smiles, tryTimes++)
+    if (tryTimes <= 5) {
+      await time.sleep(1000);
+      return await visit(smiles, tryTimes++);
     }
     console.log("❌ FAILED");
   }
@@ -37,10 +38,9 @@ async function visit(smiles, tryTimes=1) {
 
 (async () => {
   const smis = await load.load("data/example.smi");
-  console.log(smis)
   console.log("start");
   for (let i = 0; i < smis.length; i += 20) {
     await visit(smis.slice(i, i + 20));
-    await time.sleep(500)
+    await time.sleep(500);
   }
 })();
